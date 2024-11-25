f650c057f87fc1b48ef96728b5f86ab2e70d3132
Paulo Freitas <paulo.freitas.nt@gmail.com>
2024-11-25 10:23:39 -0300

    Reescrevendo a classe professor e aluno com herança

--- src/model/Aluno.java
+++ src/model/Aluno.java
@@ -2,43 +2,14 @@ package model;
 
 import java.time.LocalDate;
 
-public class Aluno {
-    private String cpf;
-    private String nome;
-    private LocalDate nascimento;
+public class Aluno extends Pessoa {
     private int matricula;
     private String curso;
 
     public Aluno(String cpf, String nome, LocalDate nascimento, int matricula, String curso) {
-        this.cpf = cpf;
-        this.nome = nome;
-        this.nascimento = nascimento;
+        super(cpf, nome, nascimento);
         this.matricula = matricula;
         this.curso = curso;
-    }
-
-    public String getCpf() {
-        return cpf;
-    }
-
-    public void setCpf(String cpf) {
-        this.cpf = cpf;
-    }
-
-    public String getNome() {
-        return nome;
-    }
-
-    public void setNome(String nome) {
-        this.nome = nome;
-    }
-
-    public LocalDate getNascimento() {
-        return nascimento;
-    }
-
-    public void setNascimento(LocalDate nascimento) {
-        this.nascimento = nascimento;
     }
 
     public int getMatricula() {
--- src/model/Professor.java
+++ src/model/Professor.java
@@ -2,43 +2,14 @@ package model;
 
 import java.time.LocalDate;
 
-public class Professor {
-    private String cpf;
-    private String nome;
-    private LocalDate nascimento;
+public class Professor extends Pessoa{
     private float salario;
     private String formacao;
 
     public Professor(String cpf, String nome, LocalDate nascimento, float salario, String formacao) {
-        this.cpf = cpf;
-        this.nome = nome;
-        this.nascimento = nascimento;
+        super(cpf,nome,nascimento);
         this.salario = salario;
         this.formacao = formacao;
-    }
-
-    public String getCpf() {
-        return cpf;
-    }
-
-    public void setCpf(String cpf) {
-        this.cpf = cpf;
-    }
-
-    public String getNome() {
-        return nome;
-    }
-
-    public void setNome(String nome) {
-        this.nome = nome;
-    }
-
-    public LocalDate getNascimento() {
-        return nascimento;
-    }
-
-    public void setNascimento(LocalDate nascimento) {
-        this.nascimento = nascimento;
     }
 
     public float getSalario() {
